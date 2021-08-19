d13dce8cc1a04f8cd36447973fa14e31893d8c97
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2021-08-19 12:16:05 -0700

    finished all it need is to build

---
+++ Final Project 7-19/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
